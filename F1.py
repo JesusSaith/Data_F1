d830c3520a5be3b8b4fefae9ecb02bb79b66d98b
SaithConD <yisuscond@gmail.com>
2023-03-09 10:02:55 -0600

    Inicio

---
+++ F1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: white;'>F1</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: white;'>Meneses Conde Jesus Saith</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: white;'>zs19004877@estudiantes.uv.mx</h3>", unsafe_allow_html=True)
